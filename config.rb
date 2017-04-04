57a5a01b48039d4905f60abcfa74b0355e8ad50f
Sunny Hong <hongmusic0204@gmail.com>
2017-04-04 23:00:56 +0800

    fix

--- config.rb
+++ config.rb
@@ -2,7 +2,7 @@ require 'compass/import-once/activate'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/"
+http_path = "/2017Spring_WebDesignHW5/"
 css_dir = "stylesheets"
 sass_dir = "sass"
 images_dir = "images"
